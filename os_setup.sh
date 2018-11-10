9c44d0111eda541c39fd5c7869807b8b5aba33ac
Lessi <kai.lessmeister@gmx.de>
2018-11-10 11:00:16 +0100

    include files added

--- os_setup.sh
+++ os_setup.sh
@@ -2,5 +2,6 @@
 
 sudo apt-get update;
 sudo apt-get upgrade;
+sudo apt-get install build-essential;
 sudo apt-get install gcc-arm-linux-gnueabi;
 
--- src/main.c
+++ src/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-
+#include "brick.h"
 
 int main (int argc, char** argv) {
-
+    ev3_init();
+    ev3_uninit();
     printf("brum\n");
 
     return 0;
