a758e29c2b19bd5420e01b5e30d076bd366a8a44
AljawadH <39992837+AljawadH@users.noreply.github.com>
2018-11-19 19:24:15 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
 #include <brick.h>
 
+#define SPEED_CIRCULAR  25
+
+#define MOTOR_LEFT OUTA
+#define MOTOR_LEFT OUTB
+#define MOTOR_BOTH ( MOTOR_LEFT | MOTOR_RIGHT )
 
 
 // gerade Linie
 void moveForward(int distance){
-    
+ 
 }
 
 //nach links drehen um eigene Achse
-void turnLeftOnPoint(int degree){
-    
+void turnLeftOnPoint(int degree)
+{
+   tacho_set_speed_sp( MOTOR_LEFT, degree );
+   tacho_set_speed_sp( MOTOR_RIGHT, -degree );
+   tacho_run_forever( MOTOR_BOTH );
 }
 
 //nach rechts drehen um eigene Achse
-void turnRightOnPoint(int degree){
-    
+void turnRightOnPoint(int degree)
+{
+   tacho_set_speed_sp( MOTOR_RIGHT, degree );
+   tacho_set_speed_sp( MOTOR_LEFT, -degree );
+   tacho_run_forever( MOTOR_BOTH );    
 }
 
 //Linkskurve
@@ -40,4 +51,3 @@ int main(){
     brick_uninit();
     return 0;
 }
-
