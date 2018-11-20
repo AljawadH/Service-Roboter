2810a3466fbc931baddc4d27958172c5fa76a452
Kai Leßmeister <lessmeister.kai@gmail.com>
2018-11-20 09:54:33 +0100

    main.c

--- src/main.c
+++ src/main.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include "ev3.h"
 #include "ev3_port.h"
-<<<<<<< HEAD
 #include "ev3_port.h"
 
-=======
->>>>>>> 7432860de3975a54746a0cb07ca72f35b4ca6e3c
 #include "brick.h"
 #include "pen.h"
 #include "movement.h"
@@ -33,10 +30,7 @@ void turnRightOnPoint(int degree)
 
 int main(){
 
-<<<<<<< HEAD
     motor_t* left_m = create_motor(LEGO_EV3_L_MOTOR, OUTA, OUTPUT_A);
-=======
->>>>>>> 7432860de3975a54746a0cb07ca72f35b4ca6e3c
 
       pen_t* pen = create_pen(LEGO_EV3_M_MOTOR);
       test_pen(pen);
@@ -47,14 +41,7 @@ int main(){
       remove_motor(left_m);
       //remove_motor(right_m);
 
-<<<<<<< HEAD
     remove_pen(pen);
-    remove_motor(left_m);
-
-
-=======
-      remove_pen(pen);
->>>>>>> 7432860de3975a54746a0cb07ca72f35b4ca6e3c
 
 
     return 0;
